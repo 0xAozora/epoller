6621557fdaec3707585ea817514fb8bcd4280273
0xAozora <atha.ar@arcor.de>
2025-04-13 01:14:58 +0200

    add windows events

--- epoll_windows.go
+++ epoll_windows.go
@@ -7,7 +7,24 @@ import (
 	"github.com/0xAozora/epoller/wepoll"
 )
 
+// TODO: Think of a better way to expose the events
 type Event = wepoll.Event
+
+const (
+	EPOLLIN      wepoll.Event = wepoll.EPOLLIN
+	EPOLLPRI                  = wepoll.EPOLLPRI
+	EPOLLOUT                  = wepoll.EPOLLOUT
+	EPOLLERR                  = wepoll.EPOLLERR
+	EPOLLHUP                  = wepoll.EPOLLHUP
+	EPOLLRDNORM               = wepoll.EPOLLRDNORM
+	EPOLLRDBAND               = wepoll.EPOLLRDBAND
+	EPOLLWRNORM               = wepoll.EPOLLWRNORM
+	EPOLLWRBAND               = wepoll.EPOLLWRBAND
+	EPOLLMSG                  = wepoll.EPOLLMSG /* Never reported. */
+	EPOLLRDHUP                = wepoll.EPOLLRDHUP
+	EPOLLONESHOT              = wepoll.EPOLLONESHOT
+)
+
 type epoll = wepoll.Epoll
 
 func NewPoller(connBufferSize int, event Event) (Poller, error) {
